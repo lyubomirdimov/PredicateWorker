364d63e5daeea72ad78848b12ddac2fa8d8fc88b
lyubomir <lyubomir.d.dimov@gmail.com>
2017-09-09 15:07:49 +0200

    small fix

--- ALEWebApp/UnitTest/UnitTests.cs
+++ ALEWebApp/UnitTest/UnitTests.cs
@@ -60,21 +60,7 @@ namespace UnitTest
         [TestMethod]
         public void CheckRegex()
         {
-            Regex r = new Regex(
-                @"^                             # Start of line
-                  (                             # Either
-                      [01A-Z](?![01A-Z])  |      # A symbol or bool followed by anything else
-                      (?<dyadic>[|&>=]\((?!,))| # Start of dyadic
-                      (?<comma-dyadic>,(?!\)))| # Looks for comma followed by dyadic. Pops off the dyadic stack.
-                      (?<dBracket-comma>\))|    # Looks for ending bracket following comma. pops off comma stack. 
-                      (?<monadic>~\((?!\)))|    # Start of monadic function.
-                      (?<uBracket-monadic>\)))  # Looks for ending bracket for unary. Pops off the monadic stack. 
-                  +                             # Any number of times.
-                  (?(dyadic)(?!))               # Assert dyadic stack is empty. All have a comma.
-                  (?(comma)(?!))                # Assert comma stack is empty. All dyadic commas followed by brackets.
-                  (?(monadic)(?!))              # Assert monadic stack is empty. All monadic expressions have closing brackets.
-                  $"
-                , RegexOptions.IgnorePatternWhitespace);
+            Regex r = LogicPropositionHelper.PropositionalRegex;
 
             List<string> validPropostions = new List<string>
             {
