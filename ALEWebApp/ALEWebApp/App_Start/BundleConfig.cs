2f4c0ae16b5efee9878090d8a6cd4960ec687714
Lyubomir <lyubomir.d.dimov@gmail.com>
2018-07-08 12:56:13 +0200

    fix libs

--- ALEWebApp/ALEWebApp/App_Start/BundleConfig.cs
+++ ALEWebApp/ALEWebApp/App_Start/BundleConfig.cs
@@ -12,23 +12,23 @@ namespace ALEWebApp
 
             // CSS style (bootstrap/style)
             bundles.Add(new StyleBundle("~/bundles/maincss").Include(
-                "~/bower_components/bootstrap/dist/css/bootstrap.min.css",
-                "~/bower_components/bootstrap/dist/css/bootstrap-reboot.min.css",
-                "~/bower_components/bootstrap/dist/css/bootstrap-grid.min.css",
-                "~/bower_components/metisMenu/dist/metisMenu.min.css",
+                "~/Content/lib/bootstrap/dist/css/bootstrap.min.css",
+                "~/Content/lib/bootstrap/dist/css/bootstrap-reboot.min.css",
+                "~/Content/lib/bootstrap/dist/css/bootstrap-grid.min.css",
+                "~/Content/lib/metisMenu/dist/metisMenu.min.css",
                 "~/Content/sass/master.min.css"));
 
             // Animate css
             bundles.Add(new StyleBundle("~/bundles/animatecss").Include(
-                "~/bower_components/animate.css/animate.min.css"));
+                "~/Content/lib/animate.css/animate.min.css"));
 
             // Font Awesome icons
             bundles.Add(new StyleBundle("~/bundles/font-awesome-css").Include(
-                "~/bower_components/font-awesome/css/font-awesome.min.css", new CssRewriteUrlTransform()));
+                "~/Content/lib/font-awesome/css/font-awesome.min.css", new CssRewriteUrlTransform()));
 
             // Jquery UI css
             bundles.Add(new StyleBundle("~/bundles/jqueryuicss").Include(
-                "~/bower_components/jquery-ui/themes/smoothness/jquery-ui.min.css"));
+                "~/Content/lib/jquery-ui/themes/smoothness/jquery-ui.min.css"));
 
             // ** PAGES
 
@@ -39,24 +39,24 @@ namespace ALEWebApp
 
             // Main website scripts
             bundles.Add(new ScriptBundle("~/bundles/mainjs").Include(
-                "~/bower_components/jQuery/dist/jquery.min.js",
-                "~/bower_components/popper.js/dist/umd/popper.min.js",
-                "~/bower_components/popper.js/dist/umd/popper-utils.min.js",
-                "~/bower_components/bootstrap/dist/js/bootstrap.min.js",
-                "~/bower_components/bootstrap/js/dist/alert.js",
-                "~/bower_components/bootstrap/js/dist/button.js",
-                "~/bower_components/bootstrap/js/dist/carousel.js",
-                "~/bower_components/bootstrap/js/dist/collapse.js",
-                "~/bower_components/bootstrap/js/dist/dropdown.js",
-                "~/bower_components/bootstrap/js/dist/modal.js",
-                "~/bower_components/bootstrap/js/dist/tooltip.js",
-                "~/bower_components/bootstrap/js/dist/popover.js",
-                "~/bower_components/bootstrap/js/dist/scrollspy.js",
-                "~/bower_components/bootstrap/js/dist/tab.js",
-                "~/bower_components/bootstrap/js/dist/util.js",
-                "~/bower_components/PACE/pace.min.js",
-                "~/bower_components/metisMenu/dist/metisMenu.min.js",
-                "~/bower_components/jquery-slimscroll/jquery.slimscroll.min.js",
+                "~/Content/lib/jQuery/dist/jquery.min.js",
+                "~/Content/lib/popper.js/dist/umd/popper.min.js",
+                "~/Content/lib/popper.js/dist/umd/popper-utils.min.js",
+                "~/Content/lib/bootstrap/dist/js/bootstrap.min.js",
+                "~/Content/lib/bootstrap/js/dist/alert.js",
+                "~/Content/lib/bootstrap/js/dist/button.js",
+                "~/Content/lib/bootstrap/js/dist/carousel.js",
+                "~/Content/lib/bootstrap/js/dist/collapse.js",
+                "~/Content/lib/bootstrap/js/dist/dropdown.js",
+                "~/Content/lib/bootstrap/js/dist/modal.js",
+                "~/Content/lib/bootstrap/js/dist/tooltip.js",
+                "~/Content/lib/bootstrap/js/dist/popover.js",
+                "~/Content/lib/bootstrap/js/dist/scrollspy.js",
+                "~/Content/lib/bootstrap/js/dist/tab.js",
+                "~/Content/lib/bootstrap/js/dist/util.js",
+                "~/Content/lib/PACE/pace.min.js",
+                "~/Content/lib/metisMenu/dist/metisMenu.min.js",
+                "~/Content/lib/jquery-slimscroll/jquery.slimscroll.min.js",
                 "~/Scripts/header.js",
                 "~/Scripts/main.js"));
 
@@ -64,12 +64,12 @@ namespace ALEWebApp
 
             // Jquery validation
             bundles.Add(new ScriptBundle("~/bundles/jqueryValidation").Include(
-                "~/bower_components/jquery-validation/dist/jquery.validate.min.js",
-                "~/bower_components/jquery-validation-unobtrusive/jquery.validate.unobtrusive.min.js"));
+                "~/Content/lib/jquery-validation/dist/jquery.validate.min.js",
+                "~/Content/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.min.js"));
 
             // Jquery UI JS
             bundles.Add(new ScriptBundle("~/bundles/jqueryuijs").Include(
-                "~/bower_components/jquery-ui/jquery-ui.min.js"
+                "~/Content/lib/jquery-ui/jquery-ui.min.js"
                 ));
 
 
